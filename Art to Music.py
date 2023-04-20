36b0fbc6ba35bb4c1a45dec5bfdfbeb9257f14e1
pavlockg <pavlocga01@sunyjcc.edu>
2023-04-20 16:12:54 -0400

    Update Art to Music.py
    Task Layout

--- Art to Music.py
+++ Art to Music.py
@@ -1,3 +1,20 @@
 # CSC 1570 project to convert Art to Music
 # Gavin Pavlock
 # 04/20/2023
+
+
+#Import our libraries
+
+#Keep trcak of music settings
+#Handle user input
+
+#See if they want to use their own image or a sample image
+#See if we have an image
+
+#Save image into a temp file
+#Ask computer to view image
+#Grab hue some HSV (Hue,Saturation,Value)
+#Separate the hues out and convert them to frequencies
+#Generic song save
+#Make an effected version of the song
+#Save the final song and make a midi 
