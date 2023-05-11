875a0ac4faff4b154903539d95fcd13457fecec5
pavlockg <pavlocga01@sunyjcc.edu>
2023-05-11 14:30:34 -0400

    Create newpage.py

---
+++ newpage.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st .set_page_config(
+        page_title="Hello",
+        page_icon="=)"
+)
+
+st.write("Welcome to Streamlit!")
+st.sidebar.success("Select a demo above.")
+st.markdown( )
