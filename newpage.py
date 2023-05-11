a0f3339226d7fb9ef3fc41685c73c901b53f51b6
pavlockg <pavlocga01@sunyjcc.edu>
2023-05-11 14:35:38 -0400

    Update newpage.py

--- newpage.py
+++ newpage.py
@@ -1,9 +1,31 @@
 import streamlit as st
 
-st .set_page_config(
-        page_title="Hello",
-        page_icon="=)"
-)
+def home():
+    st.title("Home Page")
+    st.write("Welcome to the home page!")
 
-st.title("Main Page")
-st.sidebar.success("Select a page above")
+def about():
+    st.title("About Page")
+    st.write("This is the about page.")
+
+def contact():
+    st.title("Contact Page")
+    st.write("You can contact us here.")
+
+# Dictionary mapping page names to their respective functions
+pages = {
+    "Home": home,
+    "About": about,
+    "Contact": contact
+}
+
+# Streamlit app
+def main():
+    st.sidebar.title("Navigation")
+    page = st.sidebar.selectbox("Go to", tuple(pages.keys()))
+
+    # Invoke the function based on the selected page
+    pages[page]()
+
+if __name__ == "__main__":
+    main()
