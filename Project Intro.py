004ae0da6285d8569ffb6db8144b4e24fd31a399
pavlockg <pavlocga01@sunyjcc.edu>
2023-05-11 14:54:09 -0400

    Update Project Intro.py

--- Project Intro.py
+++ Project Intro.py
@@ -4,18 +4,7 @@
 # 04/20/2023
 
 
-#Import our libraries
+# What I did to the app
 
-#Keep trcak of music settings
-#Handle user input
-
-#See if they want to use their own image or a sample image
-#See if we have an image
-
-#Save image into a temp file
-#Ask computer to view image
-#Grab hue some HSV (Hue,Saturation,Value)
-#Separate the hues out and convert them to frequencies
-#Generic song save
-#Make an effected version of the song
-#Save the final song and make a midi 
+#Changed color scheme
+#Imported my own image from the art gallery upstairs
